89d5c28435440c9c804427b6c38ad55dfecf596d
Boris Beaulant <bo@borisbeaulant.com>
2025-05-09 14:33:27 +0200

    Adding labeling + test

---
+++ include/shape/labeling.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "shape/shape.hpp"
+
+namespace shape
+{
+
+/**
+ * Computes an optimal label position for a given shape.
+ * 1/ exact calculation of the geometric center (C),
+ * 2/ search for the closest point (P) on the contour,
+ * 3/ we consider the line (CP), we search for all the segments of this line which intersect the geometry, and we take the center of the longest.
+ *
+ * @param shape The outline shape
+ * @param holes List of hole shapes
+ * @param segment_length The length of each line segment used to approximate circular arcs.
+ * @return A point representing the computed labeling position.
+ */
+Point find_label_position(
+        const Shape& shape,
+        const std::vector<Shape>& holes = {});
+
+}
